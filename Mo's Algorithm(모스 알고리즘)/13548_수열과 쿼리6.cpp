12c3b44508eab639bc3195d04420d86a3c739e45
newdeal123 <newdeal123@nate.com>
2020-01-01 16:39:00 +0900

    BOJ의 모스 알고리즘 문제들 입니다.

---
+++ Mo's Algorithm(모스 알고리즘)/13548_수열과 쿼리6.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+using namespace std;
+const int MAX_N=100000;
+int N,Q,sqrtN;
+vector<int>v;
+
+struct queryNode
+{
+    int left=0,right=0,idx=0;
+    bool operator<(const queryNode &y)const
+    {
+        if(left/sqrtN!=y.left/sqrtN)return (left/sqrtN<y.left/sqrtN);
+        else return (right<y.right);
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>N;
+    sqrtN=sqrt(N);
+    for(int i=0;i<N;i++)
+    {
+        int tmp;
+        cin>>tmp;
+        v.push_back(tmp);
+    }
+    queryNode query[MAX_N+2];
+    cin>>Q;
+    for(int i=0;i<Q;i++)
+    {
+        int left,right;
+        cin>>left>>right;
+        query[i]={left-1,right,i};
+    }
+    sort(query,query+Q);
+
+    int qAns=0,cnt[MAX_N+2]={0,},rcnt[MAX_N+2]={0,},ans[MAX_N+2]={0,};
+    int qLeft=query[0].left,qRight=query[0].right;
+
+    for(int i=qLeft;i<qRight;i++)
+        {
+            int tmp=++cnt[v[i]];
+            ++rcnt[tmp];
+            if(tmp>qAns)++qAns;
+        }
+    ans[query[0].idx]=qAns;
+
+    for(int i=1;i<Q;i++)
+    {
+        int tmp;
+        while(query[i].left<qLeft)
+        {
+            tmp=++cnt[v[--qLeft]];
+            ++rcnt[tmp];
+            if(tmp>qAns)qAns++;
+        }
+        while(query[i].right>qRight)
+        {
+            tmp=++cnt[v[qRight++]];
+            ++rcnt[tmp];
+            if(tmp>qAns)qAns++;
+        }
+        while(query[i].left>qLeft)
+        {
+            tmp=cnt[v[qLeft++]]--;
+            if(--rcnt[tmp]==0&&tmp==qAns)qAns--;
+        }
+        while(qRight>query[i].right)
+        {
+            tmp=cnt[v[--qRight]]--;
+            if(--rcnt[tmp]==0&&tmp==qAns)qAns--;
+        }
+        ans[query[i].idx]=qAns;
+    }
+    for(int i=0;i<Q;i++)cout<<ans[i]<<"\n";
+}

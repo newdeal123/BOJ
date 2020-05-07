e0756e79fe03d6c03127d19259dd79be93f275bf
newdeal123 <newdeal123@nate.com>
2020-05-07 23:42:52 +0900

    BOJ의 유니온 파인드 문제들 입니다.

--- Union-Find(유니온 파인드)/10775_공항.cpp
+++ Union-Find(유니온 파인드)/10775_공항.cpp
@@ -11,24 +11,22 @@ vector<int>v;
 struct UNION_FIND
 {
     int n;
-    vector<int>parent;
+public:
+    vector<int>parent,height,Sum;
     UNION_FIND(){;}
     UNION_FIND(int s)
     {
         n=s+1;
-        parent.resize(n);
-        for(int i=0;i<=n;i++)parent[i]=i;
+        parent.resize(n),height.resize(n,1);
+        for(int i=0;i<n;i++) parent[i]=i;
     }
     int find(int u)
     {
-        if(parent[u]==u)return u;
-        return find(parent[u]);
-    }
-    void merge(int u,int v)
-    {
-        parent[u]=v;
+        if(u==parent[u]) return u;
+        return parent[u]=find(parent[u]);
     }
 };
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -42,9 +40,8 @@ int main()
     {
         int tmp=uf.find(i);
         if(tmp==0)break;
-        uf.merge(i,uf.find(tmp-1));
+        uf.parent[tmp]=tmp-1;
         cnt++;
-        
     }
     cout<<cnt;
 

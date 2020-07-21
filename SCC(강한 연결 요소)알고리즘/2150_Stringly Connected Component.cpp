fd6d35d8e7e7a55375d382a5d8c8e0d754ed3984
newdeal123 <newdeal123@nate.com>
2020-07-21 21:52:41 +0900

    BOJ의 SCC 문제들 입니다.

--- SCC(강한 연결 요소)알고리즘/2150_Stringly Connected Component.cpp
+++ SCC(강한 연결 요소)알고리즘/2150_Stringly Connected Component.cpp
@@ -1,73 +1,69 @@
 #include <iostream>
-#include <algorithm>
-#include <string.h>
 #include <string>
+#include <string.h>
 #include <vector>
-#include <math.h>
+#include <algorithm>
 #include <stack>
+#include <unordered_map>
 using namespace std;
-vector<vector<int>> adj;
-vector<int>sccId,discoverd;
+const int MAX_N=10000+2;
+int N,M,cnt,scc,dfsN[MAX_N],sccN[MAX_N];
+vector<int>adj[MAX_N];
+bool finished[MAX_N];
 stack<int>stk;
-int sccCnt,vertexCnt,V,E;
 
-int scc(int here)
+int DFS(int curr)
 {
-    int ret=discoverd[here]=vertexCnt++;
-    stk.push(here);
-    for(auto next:adj[here])
+	dfsN[curr] = ++cnt;
+	stk.push(curr);
+	int ret = dfsN[curr];
+	for(auto next: adj[curr])
     {
-        if(discoverd[next]==-1)ret=min(ret,scc(next));
-        else if(sccId[next]==-1) ret=min(ret,discoverd[next]);
-    }
-    if(ret==discoverd[here])
-    {
-        vector<int>nowScc;
-        while(true)
+		if(dfsN[next] == 0) ret = min(ret, DFS(next));
+		else if(!finished[next]) ret = min(ret, dfsN[next]);
+	}
+	if(ret >= dfsN[curr]){
+		sccN[curr] = scc++;
+		while(true)
         {
-            int tmp=stk.top();
-            stk.pop();
-            sccId[tmp]=sccCnt;
-            if(tmp==here)break;
-        }
-        sccCnt+=1;
-    }
-    return ret;
-}
-
-vector<int> tarjanSCC()
-{
-    sccId=discoverd=vector<int>(adj.size(),-1);
-    sccCnt=vertexCnt=0;
-    for(int i=0;i<adj.size();i++)if(discoverd[i]==-1)scc(i);
-    return sccId;
+			int family = stk.top();
+			stk.pop();
+			finished[family] = true;
+			if(family == curr) break;
+			sccN[family] = sccN[curr];
+		}
+	}
+	return ret;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin>>V>>E;
-    adj.resize(V+1);
-    while(E--)
+    cin.tie(0);
+    cin>>N>>M;
+    while(M--)
     {
         int u,v;
         cin>>u>>v;
         adj[u].push_back(v);
     }
-    vector<int>ans=tarjanSCC();
-    cout<<sccCnt-1<<"\n";
-    bool visited[10000+2]={0,},flag=true;
-    for(int i=1;i<ans.size();i++)
+    memset(finished,false,sizeof(finished));
+    for(int i=1; i<=N; i++)
+		if(dfsN[i] == 0) DFS(i);
+    cout<<scc<<"\n";
+    vector<int>v[scc];
+    unordered_map<int,int>mp;
+    int mpCnt=0;
+    for(int i=1; i<=N; i++)
     {
-        if(!visited[ans[i]])
-        {
-            int target=ans[i];
-            for(int i=1;i<ans.size();i++)
-                if(target==ans[i])cout<<i<<" ";
-            cout<<"-1\n";
-            visited[ans[i]]=true;
-        }
+        if(mp.find(sccN[i])==mp.end())
+            mp.insert({sccN[i],mpCnt++});
+        v[mp[sccN[i]]].push_back(i);
+    }
+
+    for(int i=0;i<scc;i++)
+    {
+        for(auto j:v[i])cout<<j<<" ";
+        cout<<-1<<"\n";
     }
 }
